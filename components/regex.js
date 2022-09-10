de296852db65d295dd49d4768df6cca390094021
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2022-09-10 18:46:16 +0100

    Add potentialCustomElementName

--- components/regex.js
+++ components/regex.js
@@ -1,6 +1,7 @@
 export default {
 	name,
-	qName
+	qName,
+	potentialCustomElementName
 }
 
 
@@ -19,5 +20,11 @@ const UnprefixedName = String.raw`(?:${LocalPart})`;
 const PrefixedName = String.raw`(?:${Prefix}:${LocalPart})`;
 const QName = String.raw`(?:${PrefixedName}|${UnprefixedName})`;
 
+// https://html.spec.whatwg.org/multipage/custom-elements.html#valid-custom-element-name
+const PCENChar = String.raw`(?:-|\.|[0-9]|_|[a-z]|\xB7|[\u00C0-\u00D6]|[\u00D8-\u00F6]|[\u00F8-\u037D]|[\u037F-\u1FFF]|[\u200C-\u200D]|[\u203F-\u2040]|[\u2070-\u218F]|[\u2C00-\u2FEF]|[\u3001-\uD7FF]|[\uF900-\uFDCF]|[\uFDF0-\uFFFD]|[\u{10000}-\u{EFFFF}])`;
+const hyphenedElements = String.raw`(?:annotation-xml|color-profile|font-face|font-face-src|font-face-uri|font-face-format|font-face-name|missing-glyph)`;
+const PotentialCustomElementName = String.raw`(?:(?!${hyphenedElements})[a-z]${PCENChar}*-${PCENChar}*)`;
+
 export var name = new RegExp(String.raw`^${Name}$`, 'u');
 export var qName = new RegExp(String.raw`^${QName}$`, 'u');
+export var potentialCustomElementName = new RegExp(String.raw`^${PotentialCustomElementName}$`, 'u');
